e8a089de72ea49693a22a8f6925cb92fc25df060
Apoorva <aa3vs@virginia.edu>
2017-03-25 14:52:59 -0400

    test search

--- FormExample/urls.py
+++ FormExample/urls.py
@@ -8,7 +8,9 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # define the url getdata that we have written inside form
-    url(r'^getdata/', views.index),
+    # url(r'^summoner_one/', views.index),
+    url(r'summoner_one', views.summoner_one, name="summoner_one"),
+
 
     # defining the view for root URL
     url(r'^$', views.index),
--- FormExample/views.py
+++ FormExample/views.py
@@ -7,27 +7,15 @@ from django.views.decorators.csrf import csrf_exempt
 # disabling csrf (cross site request forgery)
 @csrf_exempt
 def index(request):
-    # if post request came
-    if request.method == 'POST':
-        # getting values from post
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
+    
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 
-        # adding the values in a context variable
-        context = {
-            'name': name,
-            'email': email,
-            'phone': phone
-        }
 
-        # getting our showdata template
-        template = loader.get_template('showdata.html')
+@csrf_exempt
+def summoner_one(request):
+    if request.method == 'GET':
+        template = loader.get_template('results.html')
+        query = request.GET.get('main_search', None)
 
-        # returing the template
-        return HttpResponse(template.render(context, request))
-    else:
-        # if post request is not true
-        # returing the form template
-        template = loader.get_template('index.html')
-        return HttpResponse(template.render())
+        return HttpResponse(template.render(request=request, context={'summoner_one': query}))
