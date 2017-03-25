fc17568b9929d1c585935271f20cda11b6acdd1f
Apoorva <aa3vs@virginia.edu>
2017-03-25 15:29:20 -0400

    updated UI

--- FormExample/views.py
+++ FormExample/views.py
@@ -17,7 +17,18 @@ def summoner_one(request):
     if request.method == 'GET':
         template = loader.get_template('results.html')
         summoner_one = request.GET.get('summoner_one', None)
-        sumomoner_two = request.GET.get('sumomoner_two', None)
+        summoner_two = request.GET.get('summoner_two', None)
+        summoner_three = request.GET.get('summoner_three', None)
+        summoner_four = request.GET.get('summoner_four', None)
+        summoner_five = request.GET.get('summoner_five', None)
+        summoner_six = request.GET.get('summoner_six', None)
+        summoner_seven = request.GET.get('summoner_seven', None)
+        summoner_eight = request.GET.get('summoner_eight', None)
+        summoner_nine = request.GET.get('summoner_nine', None)
+        summoner_ten = request.GET.get('summoner_ten', None)
 
-
-        return HttpResponse(template.render(request=request, context={'summoner_one': summoner_one, 'sumomoner_two': sumomoner_two}))
+        return HttpResponse(template.render(request=request, context={'summoner_one': summoner_one, 'summoner_two': summoner_two,
+                                                                        'summoner_three': summoner_three, 'summoner_four': summoner_four,
+                                                                        'summoner_five': summoner_five, 'summoner_six': summoner_six,
+                                                                        'summoner_seven': summoner_seven, 'summoner_eight': summoner_eight,
+                                                                        'summoner_nine': summoner_nine, 'summoner_ten': summoner_ten }))
