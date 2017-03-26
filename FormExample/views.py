da8d78fa890a3baecef269f1a4e11cc690d82a82
Apoorva <aa3vs@virginia.edu>
2017-03-26 00:57:01 -0400

    view all summoners and average CS

--- FormExample/views.py
+++ FormExample/views.py
@@ -19,30 +19,19 @@ def summoner_one(request):
     if request.method == 'GET':
         template = loader.get_template('results.html')
 
-        summoner_one = request.GET.get('summoner_one', None).replace(" ", "")
-        summoner_two = request.GET.get('summoner_two', None)
-        summoner_three = request.GET.get('summoner_three', None)
-        summoner_four = request.GET.get('summoner_four', None)
-        summoner_five = request.GET.get('summoner_five', None)
-        summoner_six = request.GET.get('summoner_six', None)
-        summoner_seven = request.GET.get('summoner_seven', None)
-        summoner_eight = request.GET.get('summoner_eight', None)
-        summoner_nine = request.GET.get('summoner_nine', None)
-        summoner_ten = request.GET.get('summoner_ten', None)
+        #Get Summoner Names
+        summoner_one = request.GET.get('summoner_one', None).replace(" ", "").lower()
+        summoner_two = request.GET.get('summoner_two', None).replace(" ", "").lower()
+        summoner_three = request.GET.get('summoner_three', None).replace(" ", "").lower()
+        summoner_four = request.GET.get('summoner_four', None).replace(" ", "").lower()
+        summoner_five = request.GET.get('summoner_five', None).replace(" ", "").lower()
+        summoner_six = request.GET.get('summoner_six', None).replace(" ", "").lower()
+        summoner_seven = request.GET.get('summoner_seven', None).replace(" ", "").lower()
+        summoner_eight = request.GET.get('summoner_eight', None).replace(" ", "").lower()
+        summoner_nine = request.GET.get('summoner_nine', None).replace(" ", "").lower()
+        summoner_ten = request.GET.get('summoner_ten', None).replace(" ", "").lower()
 
-        # summoner_one = summoner_one.replace(" ", "")
-        summoner_two = summoner_two.replace(" ", "")
-        summoner_three = summoner_three.replace(" ", "")
-        summoner_four = summoner_four.replace(" ", "")
-        summoner_five = summoner_five.replace(" ", "")
-        summoner_six = summoner_six.replace(" ", "")
-        summoner_seven = summoner_seven.replace(" ", "")
-        summoner_eight = summoner_eight.replace(" ", "")
-        summoner_nine = summoner_nine.replace(" ", "")
-        summoner_ten = summoner_ten.replace(" ", "")
-
-
-
+        #Get Summoner ID's
         api = RIOTAPI('RGAPI-DEBB1A9C-F681-400B-ABB1-EA9D98C88AE4')
         sum_one = api.get_summoner_by_name(summoner_one,1)
         sum_two = api.get_summoner_by_name(summoner_two,1)
@@ -57,32 +46,56 @@ def summoner_one(request):
 
 
         id1 = sum_one[summoner_one]["id"]
-        # id2 = sum_two[summoner_two]["id"]
-        # id3 = sum_three[summoner_three]["id"]
-        # id4 = sum_four[summoner_four]["id"]
-        # id5 = sum_five[summoner_five]["id"]
-        # id6 = sum_six[summoner_six]["id"]
-        # id7 = sum_seven[summoner_seven]["id"]
-        # id8 = sum_eight[summoner_eight]["id"]
-        # id9 = sum_nine[summoner_nine]["id"]
-        # id10 = sum_ten[summoner_ten]["id"]
+        id2 = sum_two[summoner_two]["id"]
+        id3 = sum_three[summoner_three]["id"]
+        id4 = sum_four[summoner_four]["id"]
+        id5 = sum_five[summoner_five]["id"]
+        id6 = sum_six[summoner_six]["id"]
+        id7 = sum_seven[summoner_seven]["id"]
+        id8 = sum_eight[summoner_eight]["id"]
+        id9 = sum_nine[summoner_nine]["id"]
+        id10 = sum_ten[summoner_ten]["id"]
 
-        a = 0
-        b = 0
-        c = 0
         info = api.get_summoner_by_name(str(id1),2)
-        # a = info["champions"][len(info["champions"])-1]["stats"]["totalSessionsPlayed"]
-        # b = info["champions"][len(info["champions"])-1]["stats"]["totalMinionKills"]
+        sum_one_cs = info["champions"][len(info["champions"])-1]["stats"]["totalMinionKills"] / info["champions"][len(info["champions"])-1]["stats"]["totalSessionsPlayed"]
+      
 
-        # c = b/a
-
-
-
+        info2 = api.get_summoner_by_name(str(id2),2)
+        sum_two_cs = info2["champions"][len(info2["champions"])-1]["stats"]["totalMinionKills"] / info2["champions"][len(info2["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info3 = api.get_summoner_by_name(str(id3),2)
+        sum_three_cs = info3["champions"][len(info3["champions"])-1]["stats"]["totalMinionKills"] / info3["champions"][len(info3["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info4 = api.get_summoner_by_name(str(id4),2)
+        sum_four_cs = info4["champions"][len(info4["champions"])-1]["stats"]["totalMinionKills"] / info4["champions"][len(info4["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info5 = api.get_summoner_by_name(str(id5),2)
+        sum_five_cs = info5["champions"][len(info5["champions"])-1]["stats"]["totalMinionKills"] / info5["champions"][len(info5["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info6 = api.get_summoner_by_name(str(id6),2)
+        sum_six_cs = info6["champions"][len(info6["champions"])-1]["stats"]["totalMinionKills"] / info6["champions"][len(info6["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info7 = api.get_summoner_by_name(str(id7),2)
+        sum_seven_cs = info7["champions"][len(info7["champions"])-1]["stats"]["totalMinionKills"] / info7["champions"][len(info7["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info8 = api.get_summoner_by_name(str(id8),2)
+        sum_eight_cs = info8["champions"][len(info8["champions"])-1]["stats"]["totalMinionKills"] / info8["champions"][len(info8["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info9 = api.get_summoner_by_name(str(id9),2)
+        sum_nine_cs = info9["champions"][len(info9["champions"])-1]["stats"]["totalMinionKills"] / info9["champions"][len(info9["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
+        info10 = api.get_summoner_by_name(str(id10),2)
+        sum_ten_cs = info10["champions"][len(info10["champions"])-1]["stats"]["totalMinionKills"] / info10["champions"][len(info10["champions"])-1]["stats"]["totalSessionsPlayed"]
+        
 
         return HttpResponse(template.render(request=request, context={'summoner_one': summoner_one, 'summoner_two': summoner_two,
                                                                         'summoner_three': summoner_three, 'summoner_four': summoner_four,
                                                                         'summoner_five': summoner_five, 'summoner_six': summoner_six,
                                                                         'summoner_seven': summoner_seven, 'summoner_eight': summoner_eight,
                                                                         'summoner_nine': summoner_nine, 'summoner_ten': summoner_ten,
-                                                                        'info': info, 'y': c}))
+                                                                        'info': info, 'sum_one_cs': sum_one_cs, 'sum_two_cs': sum_two_cs,
+                                                                        'sum_three_cs': sum_three_cs, 'sum_four_cs':sum_four_cs,
+                                                                        'sum_five_cs':sum_five_cs, 'sum_six_cs':sum_six_cs,
+                                                                        'sum_seven_cs':sum_seven_cs, 'sum_eight_cs':sum_eight_cs,
+                                                                        'sum_nine_cs':sum_nine_cs, 'sum_ten_cs':sum_ten_cs}))
 
